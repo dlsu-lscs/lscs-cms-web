63b4ed32d5e3c9c28914302ff2c39e6608977304
Ellexandrei Esponilla <xandrei37@gmail.com>
2024-10-04 20:05:06 +0800

    made nav mobile responsive

--- fronted_cms/app/page.tsx
+++ fronted_cms/app/page.tsx
@@ -4,8 +4,8 @@ import { Plus } from "lucide-react";
 
 export default function Home() {
   return (
-    <main className="px-24 pt-12">
-      <div className="flex">
+    <main className="flex justify-center">
+      <div className="flex w-10/12 mt-10 p-4 border-b-2 border-slate-800 items-center space-x-5">
         <h1 className="font-bold text-5xl">Post</h1>
         <Button className="bg-slate-800 outline-none" size="icon">
           <Plus />
