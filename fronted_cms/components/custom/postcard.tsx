d27c33d6f1afc40cfb73484a0191378ca6278f9a
Ellexandrei Esponilla <xandrei37@gmail.com>
2024-10-04 22:59:04 +0800

    finished card components for displaying posts deet

---
+++ fronted_cms/components/custom/postcard.tsx
@@ -0,0 +1,43 @@
+interface PostCardProps {
+  title: string;
+  authors: string[];
+  category: string;
+  commentsAmt: number;
+}
+
+export default function PostCard({
+  title,
+  authors,
+  category,
+  commentsAmt,
+}: PostCardProps) {
+  return (
+    <div className="flex flex-col w-full border-b-2 border-slate-800 p-4">
+      <h1 className="text-xl font-semibold">{title}</h1>
+      <div className="flex mt-2 items-center">
+        <p className="text-sm text-slate-700  pr-4">
+          Authors:{" "}
+          {authors.map((a, i) => {
+            if (i + 1 === authors.length) {
+              return a;
+            } else {
+              return a + ", ";
+            }
+          })}
+        </p>
+        <p className="text-sm text-slate-400 border-x-2 border-slate-900 px-4">
+          {category}
+        </p>
+        <p
+          className={
+            commentsAmt > 0
+              ? "text-sm text-slate-400 border-r-2 border-slate-900 px-4"
+              : "hidden"
+          }
+        >
+          {commentsAmt} comments
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ fronted_cms/components/sample_data.tsx
@@ -0,0 +1,21 @@
+export default [
+  {
+    title:
+      "UAAP87: The DLSU Green Archers falls short against the UE Red Warriors",
+    authors: ["Zel Kim", "Max Chavez"],
+    category: "Sports",
+    commentsAmt: 23,
+  },
+  {
+    title: "bgc is ugly, actually",
+    authors: ["Kim Balabas"],
+    category: "Column",
+    commentsAmt: 0,
+  },
+  {
+    title: "Sports Complex...ity",
+    authors: ["Joselito Gelilang Jr."],
+    category: "Editorial",
+    commentsAmt: 0,
+  },
+];
