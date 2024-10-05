964c47669674e71be1f2aee2385ee882eb1b7927
Ellexandrei Esponilla <xandrei37@gmail.com>
2024-10-05 11:19:09 +0800

    started working on implementing editorjs

--- fronted_cms/components/sample_data.tsx
+++ fronted_cms/components/sample_data.tsx
@@ -5,17 +5,20 @@ export default [
     authors: ["Zel Kim", "Max Chavez"],
     category: "Sports",
     commentsAmt: 23,
+    id: 1,
   },
   {
     title: "bgc is ugly, actually",
     authors: ["Kim Balabas"],
     category: "Column",
     commentsAmt: 0,
+    id: 2,
   },
   {
     title: "Sports Complex...ity",
     authors: ["Joselito Gelilang Jr."],
     category: "Editorial",
     commentsAmt: 0,
+    id: 3,
   },
 ];
